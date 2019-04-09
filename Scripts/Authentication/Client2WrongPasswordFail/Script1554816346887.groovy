4143b1c7812ff5ae992d1b2c15684411cad4e161
thaddeusphua <thaddeus.phua@gmail.com>
2019-04-09 21:49:55 +0800

    implemented complete authentication test suite

---
+++ Scripts/Authentication/Client2WrongPasswordFail/Script1554816346887.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://localhost:3000/')
+
+WebUI.click(findTestObject('Page_Conductor Support Ticket/button_Login'))
+
+WebUI.verifyElementPresent(findTestObject('Page_Sign In with Auth0/input_or_password'), 5)
+
+WebUI.delay(1)
+
+WebUI.setText(findTestObject('Object Repository/Page_Sign In with Auth0/input_or_email'), 'client2@conductor.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Sign In with Auth0/input_or_password'), 'o6CDTymj93o=')
+
+WebUI.click(findTestObject('Object Repository/Page_Sign In with Auth0/span_Log In'))
+
+WebUI.verifyElementPresent(findTestObject('Page_Sign In with Auth0/span_Wrong email or password'), 5)
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/Authentication.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Authentication</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d1028ee0-3e88-40fa-98d3-7961589bfcfe</testSuiteGuid>
+   <testCaseLink>
+      <guid>1a288feb-09b7-4e02-8d1b-b818dfe65b76</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/AdminLoginPass</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4f5f8beb-c23f-4ecd-9fcf-33ae002ab4e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/AdminWrongPasswordFail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f5859714-5a35-4908-8900-35d48cc13950</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/BaLoginPass</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>006e47b3-8fb1-4421-a406-4d340ed06a20</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/BaWrongPasswordFail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>64ff3698-5768-4aa5-9130-eb7cd011f01b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Client2LoginPass</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6ab8e493-2ea8-4e78-a5bd-30d76bc26b61</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/Client2WrongPasswordFail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>db2c35ca-222b-4c30-b677-d4f80a81332b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/ClientLoginPass</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0a41c97d-dd5e-49f6-9ce5-e526b91d324f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/ClientWrongPasswordFail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>951e767e-7fab-40b0-aa9d-ca15c8f25d7b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/DevLoginPass</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a3a4821a-9e4f-47a7-9bbb-72e807e63035</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/DevWrongPasswordFail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>383435ba-e4ad-424a-b797-0f4f8ec67534</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/TesterLoginPass</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2ecbeab4-1886-4276-9342-ef47a112fcbd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentication/TesterWrongPasswordFail</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
